a657e9226ea2610ab9483304848544255a578db1
Erich Ess <erich.g.ess@gmail.com>
2020-04-28 09:55:30 -0400

    If not `init` then check if .task dir exists before executing any subcommand

--- src/main.rs
+++ src/main.rs
@@ -25,46 +25,53 @@ fn main() {
         )
         .get_matches();
 
-    let mut tasks = match get_files(".") {
-        Err(why) => panic!("Failed to get YAML files: {}", why),
-        Ok(files) => {
-            TaskList::read_tasks(files).unwrap()
-        }
-    };
-
-    if let Some(ref matches) = args.subcommand_matches("add") {
-        let name =  matches.value_of("input").unwrap();
-        let t = Task{
-            id: tasks.next_id(),
-            name: String::from(name),
-            status: Status::Open,
-        };
-
-        let path = std::path::PathBuf::from(format!("{}.yaml", t.id));
-        match Task::write(&t, &path) {
-            Ok(_) => (),
-            Err(why) => panic!(why),
-        }
-    } else if let Some(ref done) = args.subcommand_matches("close") {
-        let id: u32 = done.value_of("ID").unwrap().parse().unwrap();
-        match tasks.get_mut(id) {
-            None => println!("No task with ID {} found", id),
-            Some(mut task) => {
-                task.status = Status::Closed;
-                let path = std::path::PathBuf::from(format!("{}.yaml", id));
-                Task::write(&task, &path).unwrap();
-            },
-        }
-    } else if args.subcommand_matches("init").is_some() {
+    if args.subcommand_matches("init").is_some() {
         match initialize() {
             Ok(InitResult::Initialized) => println!("Initialized directory"),
             Ok(InitResult::AlreadyInitialized) => println!("Already initialized"),
             Err(why) => panic!("Failed to initialize directory: {}", why),
         }
-    } else {
-        println!("list!");
-        for task in tasks.tasks {
-            println!("{:?}", task);
+    } else 
+    {
+        let task_path = match get_task_path() {
+            Some(path) => path,
+            None => panic!("Not a task project: no .task directory found in this directory or a parent dirctory"),
+        };
+
+        let mut tasks = match get_files(task_path) {
+            Err(why) => panic!("Failed to get YAML files: {}", why),
+            Ok(files) => {
+                TaskList::read_tasks(files).unwrap()
+            }
+        };
+
+        if let Some(ref matches) = args.subcommand_matches("add") {
+            let name =  matches.value_of("input").unwrap();
+            let t = Task{
+                id: tasks.next_id(),
+                name: String::from(name),
+                status: Status::Open,
+            };
+
+            let path = std::path::PathBuf::from(format!("{}/{}.yaml", task_path, t.id));
+            match Task::write(&t, &path) {
+                Ok(_) => (),
+                Err(why) => panic!(why),
+            }
+        } else if let Some(ref done) = args.subcommand_matches("close") {
+            let id: u32 = done.value_of("ID").unwrap().parse().unwrap();
+            match tasks.get_mut(id) {
+                None => println!("No task with ID {} found", id),
+                Some(mut task) => {
+                    task.status = Status::Closed;
+                    let path = std::path::PathBuf::from(format!("{}/{}.yaml", task_path, id));
+                    Task::write(&task, &path).unwrap();
+                },
+            }
+        } else {
+            for task in tasks.tasks {
+                println!("{:?}", task);
+            }
         }
     }
 }
@@ -96,6 +103,13 @@ fn initialize() -> std::io::Result<InitResult> {
                 Ok(_) => Ok(InitResult::Initialized),
             }
         }
+}
+
+fn get_task_path() -> Option<&'static str> {
+    match std::fs::read_dir("./.task") {
+        Ok(_) => Some("./.task"),
+        Err(_) => None,
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
