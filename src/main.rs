2c161a24fe24a9b6d206b3359c090467bec64ede
Erich Ess <erich.g.ess@gmail.com>
2020-04-27 20:38:12 -0400

    Add a TaskList type for managing list of tasks

--- src/main.rs
+++ src/main.rs
@@ -22,47 +22,56 @@ fn main() {
         )
         .get_matches();
 
+    let mut tasks = match get_files(".") {
+        Err(why) => panic!("Failed to get YAML files: {}", why),
+        Ok(files) => {
+            TaskList::read_tasks(files).unwrap()
+        }
+    };
+
     if let Some(ref matches) = args.subcommand_matches("add") {
         let name =  matches.value_of("input").unwrap();
         let t = Task{
-            id: 1,
+            id: tasks.next_id(),
             name: String::from(name),
             status: Status::Open,
         };
 
-        match Task::write(&t, "1.yaml") {
+        let path = std::path::PathBuf::from(format!("{}.yaml", t.id));
+        match Task::write(&t, &path) {
             Ok(_) => (),
             Err(why) => panic!(why),
         }
     } else if let Some(ref done) = args.subcommand_matches("close") {
-        let id = done.value_of("ID").unwrap();
-        let path = format!("{}.yaml", id);
-        match Task::read(path.as_str()) {
-            Ok(mut t) => {
-                t.status = Status::Closed;
-                Task::write(&t, path.as_str()).unwrap();
+        let id: u32 = done.value_of("ID").unwrap().parse().unwrap();
+        match tasks.get_mut(id) {
+            None => println!("No task with ID {} found", id),
+            Some(mut task) => {
+                task.status = Status::Closed;
+                let path = std::path::PathBuf::from(format!("{}.yaml", id));
+                Task::write(&task, &path).unwrap();
             },
-            Err(why) => panic!(why),
         }
-        println!("{}", id);
     } else {
         println!("list!");
-        get_files(".");
+        for task in tasks.tasks {
+            println!("{:?}", task);
+        }
     }
 }
 
-fn get_files(path: &str) -> std::io::Result<()> {
+fn get_files(path: &str) -> std::io::Result<Vec<std::path::PathBuf>> {
     use std::fs;
 
     let contents = fs::read_dir(path)?;
     let yaml_files = contents.filter(|f| f.as_ref().unwrap().path().extension().map(|e| e == "yaml").unwrap_or(false));
+    let mut files = vec![];
     for yaml in yaml_files {
         let file = yaml?;
-        let task = Task::read(file.path().to_str().unwrap());
-        println!("{:?}", task);
+        files.push(file.path());
     }
 
-    Ok(())
+    Ok(files)
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -79,7 +88,7 @@ struct Task {
 }
 
 impl Task {
-    fn write(task: &Task, path: &str) -> std::io::Result<()> {
+    fn write(task: &Task, path: &std::path::PathBuf) -> std::io::Result<()> {
         let mut file = File::create(path)?;
 
         let s = serde_yaml::to_string(task).unwrap();
@@ -87,7 +96,7 @@ impl Task {
         file.write_all(s.as_bytes())
     }
 
-    fn read(path: &str) -> std::io::Result<Task> {
+    fn read(path: &std::path::PathBuf) -> std::io::Result<Task> {
         let mut file = File::open(path)?;
 
         let mut s = String::new();
@@ -95,5 +104,43 @@ impl Task {
 
         let y = serde_yaml::from_str::<Task>(&s).unwrap();
         Ok(y)
+    }
+
+}
+
+struct TaskList {
+    tasks: Vec<Task>,
+}
+
+impl TaskList {
+    fn read_tasks(paths: Vec<std::path::PathBuf>) -> std::io::Result<TaskList> {
+        let mut tasks = vec![];
+        for path in paths.into_iter() {
+            let task = Task::read(&path)?;
+            tasks.push(task);
+        }
+        Ok(TaskList{tasks})
+    }
+
+    fn next_id(&self) -> u32 {
+        if self.tasks.len() == 0 {
+            1
+        } else {
+            let mut largest_id = self.tasks[0].id;
+            for task in self.tasks.iter() {
+                if task.id > largest_id {
+                    largest_id = task.id;
+                }
+            }
+            largest_id+1
+        }
+    }
+
+    fn get(&self, id: u32) -> Option<&Task> {
+        self.tasks.iter().find(|t| t.id == id)
+    }
+
+    fn get_mut(&mut self, id: u32) -> Option<&mut Task> {
+        self.tasks.iter_mut().find(|t| t.id == id)
     }
 }
