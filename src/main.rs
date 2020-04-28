bd7de851dcebf96617f66011a5e62707abaa10cb
Erich Ess <erich.g.ess@gmail.com>
2020-04-28 10:33:21 -0400

    When searching for ".task" check to make sure the file is a directory

--- src/main.rs
+++ src/main.rs
@@ -112,6 +112,43 @@ fn get_task_path() -> Option<&'static str> {
     }
 }
 
+fn up_search(file_name: &str) -> std::io::Result<Option<std::path::PathBuf>> {
+    let path = std::fs::canonicalize(".")?;
+
+    let mut found = None;
+
+    for parent in path.ancestors() {
+        let mut files = parent.read_dir()?;
+        found = files.find(|f| {
+            let file = f.as_ref().unwrap();
+            let meta = file.metadata();
+            match meta {
+                Ok(md) => {
+                    let ty = md.file_type();
+                    if ty.is_dir() {
+                        file.file_name() == file_name
+                    } else {
+                        false
+                    }
+                },
+                Err(_) => false
+            }
+        });
+
+        if found.is_some() {
+            break;
+        }
+    }
+
+    match found {
+        Some(result) => match result {
+            Err(why) => Err(why),
+            Ok(v) => Ok(Some(v.path())),
+        },
+        None => Ok(None),
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 enum Status {
     Open,
